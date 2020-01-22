1e1118103571c37b7364aad2d7565bde082afbad
Suman Mahato <sumanmahato0099@gmail.com>
2020-01-22 23:46:13 +0530

    Course and Bootcamp relationship established

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)/g, match => `$${match}`);
 
     // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
     // Select Fields
     if (req.query.select) {
@@ -125,7 +125,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
         return next(
             new ErrorResponse(
@@ -134,6 +134,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
             )
         );
     }
+
+    bootcamp.remove();
+
     res.status(200).json({ success: true, data: {} });
 });
 
