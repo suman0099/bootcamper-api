127d3875a1220daf4fd9cb4d5ce1dc76a57c0fd8
Suman Mahato <sumanmahato0099@gmail.com>
2020-01-28 20:06:15 +0530

    Logout route added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        // Set token from Bearer token in header
         token = req.headers.authorization.split(' ')[1];
+        // Set token from cookie
     } else if (req.cookies.token) {
         token = req.cookies.token;
     }
@@ -19,7 +21,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     // Make sure token exists
     if (!token) {
         return next(
-            new ErrorResponse('Not authorize to access this route', 401)
+            new ErrorResponse('Not authorized to access this route', 401)
         );
     }
 
@@ -34,7 +36,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
         next();
     } catch (err) {
         return next(
-            new ErrorResponse('Not authorize to access this route', 401)
+            new ErrorResponse('Not authorized to access this route', 401)
         );
     }
 });
